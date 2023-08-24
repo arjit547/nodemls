dbe20d1f75f830fa064e21619c18d2b5c80428e3
Arjit Chauhan <ChauhanArjit@SEASIAINFOTECH.COM>
2023-08-24 23:21:15 +0530

    mls

---
+++ after-install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/ubuntu/nodemls
+npm install express --save
+npm install -g pm2
+
---
+++ before-install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /home/ubuntu/nodemls
+rm -rf node_modules
+rm -f package-lock.json
+
+
