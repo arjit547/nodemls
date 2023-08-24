11ae7d7737deaeca80201a41654dbf4b8bbf000e
arjit547 <97424724+arjit547@users.noreply.github.com>
2023-08-25 00:30:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello LMS from devops');
+  res.send('Hello LMS from devops123');
 });
 app.listen(4000, function () {
   console.log('Example app listening on port 4000!');
